0d38d558de108a0beb7bc0e002b9a4034e291ce2
Sam Plews <the@codeite.com>
2015-05-11 22:37:31 +0100

    Add CORS

--- routes/up.js
+++ routes/up.js
@@ -4,10 +4,15 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res) {
 
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,X-Requested-With,Authorization,Access-Control-Allow-Origin');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,GET,DELETE');
+
   if(false) {
     var delay = 5000 + ~~(Math.random() * 5000);
 
     setTimeout(function () {
+      
       res.send('');
     }, delay);
     return;
